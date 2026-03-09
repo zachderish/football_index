d43f8e178f8a770691b75e26b096ac0914cd6c0d
Zachary <zderish@gmail.com>
2026-03-09 17:55:00 -0400

    Typescript tutorial - section 1

---
+++ app.js
@@ -0,0 +1,6 @@
+var message = 'Hello, TypeScript!';
+// create a new heading element
+var heading = document.createElement('h1');
+heading.textContent = message;
+// add heading to document
+document.body.appendChild(heading);
---
+++ app.ts
@@ -0,0 +1,8 @@
+let message: string = 'Hello, TypeScript!';
+
+// create a new heading element
+let heading = document.createElement('h1');
+heading.textContent = message;
+
+// add heading to document
+document.body.appendChild(heading);
